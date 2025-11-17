ab7ee162a94b4e42b592bdc8ce5927441489868b
seokjinjuseyyo <seokjinjuseyyo1@gmail.com>
2025-11-17 08:20:59 +0700

    to

--- Utama/main.py
+++ Utama/main.py
@@ -1,111 +1,114 @@
-# # import os
-# # import time
-# # from prettytable import PrettyTable
+import os
+import time
+from admin import *
+from user import *
+from fungsi import *
+from prettytable import PrettyTable
 
-# # while True :
+while True :
 
-# #     loading()
-# #     tampilkan_header_utama()
-# #     menu_awal()
+    loading()
+    tampilkan_header_utama()
+    menu_awal()
 
-# #     pilihan_utama = input("Pilih menu (1-3): ")
+    pilihan_utama = input("Pilih menu (1-3): ")
 
-# #     if pilihan_utama == "1":
-# #                 layar_bersih()
-# #                 login()
-# #                 pilihan_login = input("Pilih menu (1-2): ")
+    if pilihan_utama == "1":
+                layar_bersih()
+                login()
+                pilihan_login = input("Pilih menu (1-2): ")
 
-# #                 if pilihan_login == "1" : # Login Admin
-# #                     layar_bersih()
-# #                     tampilkan_header_utama()
+                if pilihan_login == "1" : # Login Admin
+                    layar_bersih()
+                    tampilkan_header_utama()
                 
-# #                     while percobaan < max_percobaan:
-# #                             Login_Username = input("Masukkan username Admin : ")
-# #                             Login_Password = input("Masukan password Admin : ")
+                    while percobaan < max_percobaan:
+                            Login_Username = input("Masukkan username Admin : ")
+                            Login_Password = input("Masukan password Admin : ")
 
 
-# #                             if Login_Username == Username and Login_Password == Password  :
+                            if Login_Username == Username and Login_Password == Password  :
                 
-# #                                 Admin = True
-# #                                 status_login = True
-# #                                 break
+                                Admin = True
+                                status_login = True
+                                break
                                     
 
-# #                             else:
-# #                                 percobaan += 1
-# #                                 print("Username atau Password salah !!!")
-# #                                 layar_bersih()
+                            else:
+                                percobaan += 1
+                                print("Username atau Password salah !!!")
+                                layar_bersih()
 
-# #                                 if percobaan == max_percobaan :
-# #                                     layar_bersih()
-# #                                     print("Username atau Password salah !!!")
-# #                                     input("Tekan Enter untuk memulai dari awal......")
-# #                                     break
+                                if percobaan == max_percobaan :
+                                    layar_bersih()
+                                    print("Username atau Password salah !!!")
+                                    input("Tekan Enter untuk memulai dari awal......")
+                                    break
                     
-# #                 if pilihan_login == "2" : # Login User
-# #                     layar_bersih()
-# #                     tampilkan_header_utama()
+                if pilihan_login == "2" : # Login User
+                    layar_bersih()
+                    tampilkan_header_utama()
 
-# #                     while percobaan < max_percobaan:
-# #                         Login_Username = input("Masukkan username : ")
-# #                         Login_Password = input("Masukan password : ")
+                    while percobaan < max_percobaan:
+                        Login_Username = input("Masukkan username : ")
+                        Login_Password = input("Masukan password : ")
 
 
-# #                         if Login_Username == pengguna and Login_Password == Password  :
+                        if Login_Username == pengguna and Login_Password == Password  :
             
-# #                             User = True
-# #                             status_login = True
-# #                             break
+                            User = True
+                            status_login = True
+                            break
                                 
 
-# #                         else:
-# #                             percobaan += 1
-# #                             print("Username atau Password salah !!!")
-# #                             layar_bersih()
+                        else:
+                            percobaan += 1
+                            print("Username atau Password salah !!!")
+                            layar_bersih()
 
-# #                             if percobaan == max_percobaan :
-# #                                 layar_bersih()
-# #                                 print("Username atau Password salah !!!")
-# #                                 input("Tekan Enter untuk memulai dari awal......")
-# #                                 break
+                            if percobaan == max_percobaan :
+                                layar_bersih()
+                                print("Username atau Password salah !!!")
+                                input("Tekan Enter untuk memulai dari awal......")
+                                break
 
-#      while Admin == True :
-#                     while status_login == True:
-#                         layar_bersih()
-#                         menu_admin()
+                    while Admin == True :
+                    while status_login == True:
+                        layar_bersih()
+                        menu_admin()
 
-#                         pilihan_admin = input("Pilih menu (1-6): ")
+                        pilihan_admin = input("Pilih menu (1-6): ")
 
-#                         if pilihan_admin == "1":   # Lihat menu
-#                             layar_bersih()
-#                             tampilkan_daftar_produk() # ini pakai prosedur 1
+                        if pilihan_admin == "1":   # Lihat menu
+                            layar_bersih()
+                            tampilkan_daftar_produk() # ini pakai prosedur 1
                         
-#                         elif pilihan_admin == "2":   # Update
-#                             layar_bersih()
-#                             update()
+                        elif pilihan_admin == "2":   # Update
+                            layar_bersih()
+                            update()
                         
-#                         elif pilihan_admin == "3":   # Edit
-#                             layar_bersih()
-#                             edit()
+                        elif pilihan_admin == "3":   # Edit
+                            layar_bersih()
+                            edit()
 
-#                         elif pilihan_admin == "4":   # Edit
-#                             layar_bersih()
-#                             hapus()
+                        elif pilihan_admin == "4":   # Edit
+                            layar_bersih()
+                            hapus()
                         
-#                         elif pilihan_admin == "5":   # Cari
-#                             layar_bersih()
-#                             print("=" * 59)
-#                             print("|                      CARI PRODUK                        |")
-#                             print("=" * 59)
-#                             keyword = input("Masukkan nama produk: ")
+                        elif pilihan_admin == "5":   # Cari
+                            layar_bersih()
+                            print("=" * 59)
+                            print("|                      CARI PRODUK                        |")
+                            print("=" * 59)
+                            keyword = input("Masukkan nama produk: ")
                             
-#                             cari_produk_dan_tampilkan(keyword)
+                            cari_produk_dan_tampilkan(keyword)
                             
-#                         elif pilihan_admin == "6":
-#                                 layar_bersih()
-#                                 print("Logout berhasil!")
-#                                 Admin = False
-#                                 status_login = False
+                        elif pilihan_admin == "6":
+                                layar_bersih()
+                                print("Logout berhasil!")
+                                Admin = False
+                                status_login = False
                         
-#                         else:
-#                             print("Pilihan tidak valid!")
+                        else:
+                            print("Pilihan tidak valid!")
--- Utama/user.py
+++ Utama/user.py
@@ -1,80 +1,80 @@
-# import os
-# import time
-# from prettytable import PrettyTable
+import os
+import time
+from prettytable import PrettyTable
 
-# def menu_user() :
-#         print("=" * 50)
-#         print(f"|           MENU PELANGGAN - Halo, !          |")
-#         print("=" * 50)
-#         print("\n1. Lihat Semua Produk")
-#         print("2. Cari Produk")
-#         print("3. Tambah ke Keranjang")
-#         print("4. Lihat Keranjang")
-#         print("5. Hapus dari Keranjang")
-#         print("6. Checkout")
-#         print("7. Logout")
+def menu_user() :
+        print("=" * 50)
+        print(f"|           MENU PELANGGAN - Halo, !          |")
+        print("=" * 50)
+        print("\n1. Lihat Semua Produk")
+        print("2. Cari Produk")
+        print("3. Tambah ke Keranjang")
+        print("4. Lihat Keranjang")
+        print("5. Hapus dari Keranjang")
+        print("6. Checkout")
+        print("7. Logout")
 
-# def tambah_keranjang () :
-#                 print("=" * 59)
-#                 print("|                  TAMBAH KE KERANJANG                    |")
-#                 print("=" * 59)
+def tambah_keranjang () :
+                print("=" * 59)
+                print("|                  TAMBAH KE KERANJANG                    |")
+                print("=" * 59)
                 
-#                 # Menggunakan PrettyTable untuk menampilkan list produk
-#                 table = PrettyTable()
-#                 table.field_names = ["ID", "Nama Produk", "Kategori", "Harga", "Stok"]
-#                 table.align["ID"] = "c"
-#                 table.align["Nama Produk"] = "l"
-#                 table.align["Kategori"] = "l"
-#                 table.align["Harga"] = "r"
-#                 table.align["Stok"] = "c"
+                # Menggunakan PrettyTable untuk menampilkan list produk
+                table = PrettyTable()
+                table.field_names = ["ID", "Nama Produk", "Kategori", "Harga", "Stok"]
+                table.align["ID"] = "c"
+                table.align["Nama Produk"] = "l"
+                table.align["Kategori"] = "l"
+                table.align["Harga"] = "r"
+                table.align["Stok"] = "c"
                 
-#                 for id_produk, data in produk.items():
-#                         table.add_row([
-#                                 id_produk,
-#                                 data['nama'],
-#                                 data['kategori'],
-#                                 f"Rp {data['harga']:,}",
-#                                 data['stok']
-#                         ])
+                for id_produk, data in produk.items():
+                        table.add_row([
+                                id_produk,
+                                data['nama'],
+                                data['kategori'],
+                                f"Rp {data['harga']:,}",
+                                data['stok']
+                        ])
                 
-#                 print(table)
+                print(table)
                 
-#                 try:
-#                         global keranjang
+                try:
+                        global keranjang
                 
-#                         id_beli = validasi_input_angka("\nMasukkan ID produk yang ingin dibeli: ", "ID harus berupa angka!")
-#                         if id_beli is None:
-#                                 raise ValueError("Input dibatalkan")
+                        id_beli = validasi_input_angka("\nMasukkan ID produk yang ingin dibeli: ", "ID harus berupa angka!")
+                        if id_beli is None:
+                                raise ValueError("Input dibatalkan")
                         
-#                         if id_beli not in produk:
-#                                 raise KeyError("Produk dengan ID tersebut tidak ditemukan!")
+                        if id_beli not in produk:
+                                raise KeyError("Produk dengan ID tersebut tidak ditemukan!")
                         
-#                         jumlah_beli = validasi_input_angka("Jumlah: ", "Jumlah harus berupa angka!")
-#                         if jumlah_beli is None or jumlah_beli <= 0:
-#                                 raise ValueError("Jumlah harus lebih dari 0")
+                        jumlah_beli = validasi_input_angka("Jumlah: ", "Jumlah harus berupa angka!")
+                        if jumlah_beli is None or jumlah_beli <= 0:
+                                raise ValueError("Jumlah harus lebih dari 0")
                         
-#                         if jumlah_beli > produk[id_beli]["stok"]:
-#                                 raise ValueError(f"Stok tidak cukup! Stok tersedia: {produk[id_beli]['stok']}")
+                        if jumlah_beli > produk[id_beli]["stok"]:
+                                raise ValueError(f"Stok tidak cukup! Stok tersedia: {produk[id_beli]['stok']}")
                         
-#                         # Inisialisasi keranjang user jika belum ada
-#                         if user_login not in keranjang:
-#                                 keranjang[user_login] = {}
+                        # Inisialisasi keranjang user jika belum ada
+                        if user_login not in keranjang:
+                                keranjang[user_login] = {}
                         
-#                         # Tambah ke keranjang
-#                         if id_beli in keranjang[user_login]:
-#                                 keranjang[user_login][id_beli]["jumlah"] += jumlah_beli
-#                         else:
-#                                 keranjang[user_login][id_beli] = {
-#                                 "nama": produk[id_beli]["nama"],
-#                                 "harga": produk[id_beli]["harga"],
-#                                 "jumlah": jumlah_beli
-#                                 }
+                        # Tambah ke keranjang
+                        if id_beli in keranjang[user_login]:
+                                keranjang[user_login][id_beli]["jumlah"] += jumlah_beli
+                        else:
+                                keranjang[user_login][id_beli] = {
+                                "nama": produk[id_beli]["nama"],
+                                "harga": produk[id_beli]["harga"],
+                                "jumlah": jumlah_beli
+                                }
                         
-#                         print(f"\n{produk[id_beli]['nama']} (x{jumlah_beli}) berhasil ditambahkan ke keranjang!")
-#                         time.sleep(4)
-#                         print("=" * 59)
+                        print(f"\n{produk[id_beli]['nama']} (x{jumlah_beli}) berhasil ditambahkan ke keranjang!")
+                        time.sleep(4)
+                        print("=" * 59)
                 
-#                 except (ValueError, KeyError) as e:
-#                         print(f"\nError: {e}")
-#                         print("=" * 59)
-#                         time.sleep(4)
+                except (ValueError, KeyError) as e:
+                        print(f"\nError: {e}")
+                        print("=" * 59)
+                        time.sleep(4)
